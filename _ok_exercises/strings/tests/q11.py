4a903ab06d5ecd3cfeb1f6cd4d1e82c9638ecb67
Matthew Brett <matthew.brett@gmail.com>
2019-10-23 11:46:17 +0100

    Add strings exercise.

---
+++ _ok_exercises/strings/tests/q11.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question',
+  'points': 10,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> new_year
+          2020
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
